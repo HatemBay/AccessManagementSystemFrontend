9aec7f9061c7cc6ffc9aa335db1f7668626293c0
hatemBay <bayoudhhatem5@gmail.com>
2023-05-20 00:30:45 +0100

    added search for demands list/ added simple frontend pagination to demands and clients

--- src/app/pages/admin/admin-demand-list/admin-demand-list.component.ts
+++ src/app/pages/admin/admin-demand-list/admin-demand-list.component.ts
@@ -3,49 +3,70 @@ import { NavigationExtras, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { Demand } from 'src/app/services/demand/demand';
 import { DemandService } from 'src/app/services/demand/demand.service';
-import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin-demand-list',
   templateUrl: './admin-demand-list.component.html',
-  styleUrls: ['./admin-demand-list.component.scss']
+  styleUrls: ['./admin-demand-list.component.scss'],
 })
 export class AdminDemandListComponent {
   //@ts-ignore
-  @ViewChild("editModal") editModal: TemplateRef<any>;
+  @ViewChild('editModal') editModal: TemplateRef<any>;
+  filter: boolean = false;
+  currPage: number = 1;
+  input: string = '';
   demands: Demand[] = [];
-  myEmail = localStorage.getItem("user-email");
+  temp: Demand[] = [];
+  pages: number;
+  tempPages: number;
+  nbOfElements: number = 2;
+  myEmail = localStorage.getItem('user-email');
   selectedDemand: Demand | undefined;
   demand: any = {
     refusalReason: '',
-    notes: ''
+    notes: '',
   };
 
-  constructor(private demandService: DemandService, private router: Router, private authService: AuthService, public modalService: NgbModal,
-    public activeModal: NgbActiveModal) { }
+  constructor(
+    private demandService: DemandService,
+    private router: Router,
+    private authService: AuthService,
+    public modalService: NgbModal,
+    public activeModal: NgbActiveModal
+  ) {}
 
   ngOnInit() {
-    this.getAll();
-
+    this.getAll(1);
   }
 
-  getAll() {
+  //Lazy to make a solid backend pagination :)
+  getAll(page: number) {
     const id = this.authService.getUserDetails().id;
     if (id) {
       this.demandService.findAll().subscribe((data) => {
-        console.log(data);
+        this.demands = data.filter((val) => val.state === null).reverse();
 
-        this.demands = data.filter(val => val.state === null);
-        this.demands = this.demands.reverse();
+        this.temp = [...this.demands].slice(
+          (page - 1) * this.nbOfElements,
+          (page - 1) * this.nbOfElements + this.nbOfElements
+        );
+        if (this.demands.length > 0) {
+          this.pages = this.tempPages = Math.ceil(
+            this.demands.length / this.nbOfElements
+          );
+        } else {
+          this.pages = this.tempPages = 1;
+        }
       });
     }
   }
 
   accept(demand: Demand, id: number) {
     demand.state = true;
-    this.demandService.update(demand, id).subscribe(data => {
-      alert("Demand Accepted");
-      this.getAll();
+    this.demandService.update(demand, id).subscribe((data) => {
+      alert('Demand Accepted');
+      this.getAll(1);
     });
   }
 
@@ -55,34 +76,106 @@ export class AdminDemandListComponent {
   }
 
   dismissModal(modal: any) {
-    modal.dismiss("Cross click");
+    modal.dismiss('Cross click');
   }
 
   toRefuseForm(id: any) {
-    this.modalService.open
+    this.modalService.open;
     var navigationExtras: NavigationExtras = {
       queryParams: {
         demandId: id,
       },
     };
 
-    this.router.navigate(["/demand-refuse"], navigationExtras);
+    this.router.navigate(['/demand-refuse'], navigationExtras);
   }
 
   refuseDemand(modal: any) {
-    console.log(this.demand);
-    console.log(this.selectedDemand);
     if (this.selectedDemand && this.selectedDemand.id) {
       this.selectedDemand.refusalReason = this.demand.refusalReason;
       this.selectedDemand.notes = this.demand.notes;
       this.selectedDemand.state = false;
 
-      this.demandService.update(this.selectedDemand, this.selectedDemand.id).subscribe(() => {
-        alert("Demand Refused");
-        this.demand = {};
-        this.dismissModal(modal);
-        this.getAll();
-      })
+      this.demandService
+        .update(this.selectedDemand, this.selectedDemand.id)
+        .subscribe(() => {
+          alert('Demand Refused');
+          this.demand = {};
+          this.dismissModal(modal);
+          this.getAll(1);
+        });
     }
+  }
+
+  search(input: string, page: number) {
+    this.input = input;
+    this.filter = true;
+    console.log(this.demands[2].user.companyName);
+
+    this.temp = this.demands.filter((demand) => {
+      const createdAt = new Date(demand.created_at);
+      const visitDayStart = new Date(demand.visitDayStart);
+      const visitDayEnd = new Date(demand.visitDayEnd);
+      const createdAtFormatted =
+        createdAt.getDate() +
+        '/' +
+        '0' +
+        (createdAt.getMonth() + 1) +
+        '/' +
+        createdAt.getFullYear();
+      const visitDayStartFormatted =
+        visitDayStart.getDate() +
+        '/' +
+        '0' +
+        (visitDayStart.getMonth() + 1) +
+        '/' +
+        visitDayStart.getFullYear();
+      const visitDayEndFormatted =
+        visitDayEnd.getDate() +
+        '/' +
+        '0' +
+        (visitDayEnd.getMonth() + 1) +
+        '/' +
+        visitDayEnd.getFullYear();
+      return (
+        demand.user.name.indexOf(input) !== -1 ||
+        demand.user.companyName.toString().indexOf(input) !== -1 ||
+        demand.concernedLocal.indexOf(input) !== -1 ||
+        demand.concernedSite.indexOf(input) !== -1 ||
+        createdAtFormatted.indexOf(input) !== -1 ||
+        visitDayStartFormatted.indexOf(input) !== -1 ||
+        visitDayEndFormatted.indexOf(input) !== -1 ||
+        demand.demandObject.indexOf(input) !== -1
+      );
+    });
+
+    //number pagination
+    if (this.temp.length > 0) {
+      this.tempPages = Math.ceil(this.temp.length / this.nbOfElements);
+    } else {
+      this.tempPages = 1;
+    }
+    //extracting the data we need to show in page
+    this.temp = this.temp.slice(
+      (page - 1) * this.nbOfElements,
+      (page - 1) * this.nbOfElements + this.nbOfElements
+    );
+
+    //reset
+    if (input.length === 0) {
+      this.filter = false;
+      this.temp = this.demands.slice(
+        (page - 1) * this.nbOfElements,
+        (page - 1) * this.nbOfElements + this.nbOfElements
+      );
+      this.tempPages = this.pages;
+    }
+  }
+  goToPage(page: number) {
+    this.currPage = page;
+    //if there is a word in the search case we will paginate through the search results / else we paginate through the normal values
+    if (this.filter) {
+      this.search(this.input, page);
+    } else this.getAll(page);
   }
 }
--- src/app/pages/admin/customers-list/customers-list.component.ts
+++ src/app/pages/admin/customers-list/customers-list.component.ts
@@ -9,28 +9,49 @@ import { NavigationExtras, Router } from '@angular/router';
   styleUrls: ['./customers-list.component.scss'],
 })
 export class CustomersListComponent {
+  filter: boolean = false;
+  input: string = '';
+  currPage: number = 1;
+  pages: number;
+  tempPages: number;
+  nbOfElements: number = 2;
   customers: Customer[] = [];
   temp: Customer[] = [];
-  myEmail = localStorage.getItem("user-email");
+  myEmail = localStorage.getItem('user-email');
 
-  constructor(private customerService: CustomerService, private router: Router) { }
+  constructor(
+    private customerService: CustomerService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.getAll();
+    this.getAll(1);
   }
 
-  getAll() {
+  //Lazy to make a solid backend pagination :)
+  getAll(page: number) {
     this.customerService.findAll().subscribe((data) => {
-      this.customers = data;
-      this.temp = data;
-      this.customers = this.customers.reverse();
+      this.customers = data.reverse();
+      this.temp = data
+        .reverse()
+        .slice(
+          (page - 1) * this.nbOfElements,
+          (page - 1) * this.nbOfElements + this.nbOfElements
+        );
+      if (this.customers.length > 0) {
+        this.pages = this.tempPages = Math.ceil(
+          this.customers.length / this.nbOfElements
+        );
+      } else {
+        this.pages = this.tempPages = 1;
+      }
     });
   }
 
   del(customer: Customer) {
-    this.customerService.delete(customer).subscribe(data => {
-      alert("User deleted Successfully");
-      this.getAll();
+    this.customerService.delete(customer).subscribe((data) => {
+      alert('User deleted Successfully');
+      this.getAll(1);
     });
   }
 
@@ -41,18 +62,49 @@ export class CustomersListComponent {
       },
     };
 
-    this.router.navigate(["/modifycustomer"], navigationExtras);
+    this.router.navigate(['/modifycustomer'], navigationExtras);
   }
 
-  search(input: string) {
-    this.temp = this.customers.filter((customer) =>
-      customer.name.indexOf(input) !== -1 ||
-      customer.cin.indexOf(input) !== -1 ||
-      customer.email.indexOf(input) !== -1 ||
-      customer.role.indexOf(input) !== -1
-    )
-    if (input.length === 0) {
-      this.temp = this.customers;
+  search(input: string, page: number) {
+    this.filter = true;
+    this.input = input;
+    this.temp = this.customers.filter((customer) => {
+      return (
+        customer.name.indexOf(input) !== -1 ||
+        customer.cin.indexOf(input) !== -1 ||
+        customer.email.indexOf(input) !== -1 ||
+        customer.role.indexOf(input) !== -1
+      );
+    });
+
+    //number pagination
+    if (this.temp.length > 0) {
+      this.tempPages = Math.ceil(this.temp.length / this.nbOfElements);
+    } else {
+      this.tempPages = 1;
     }
+    //extracting the data we need to show in page
+    this.temp = this.temp.slice(
+      (page - 1) * this.nbOfElements,
+      (page - 1) * this.nbOfElements + this.nbOfElements
+    );
+
+    //reset
+    if (input.length === 0) {
+      this.filter = false;
+      this.temp = this.customers.slice(
+        (page - 1) * this.nbOfElements,
+        (page - 1) * this.nbOfElements + this.nbOfElements
+      );
+      this.tempPages = this.pages;
+    }
+  }
+
+  goToPage(page: number) {
+    this.currPage = page;
+    //if there is a word in the search case we will paginate through the search results / else we paginate through the normal values
+    if (this.filter) {
+      this.search(this.input, page);
+    } else this.getAll(page);
   }
 }
